61e114fc0312d4e90e582ccaa9903050b957f6aa
Henrique Chaves <henriquechavesmm@gmail.com>
2020-05-24 04:05:47 -0300

    Evolution Router v2
    * Banco de dados alterado: foram criadas novas colunas, loja e categoria
    
    * Funcionalidades novas: poder cadastrar lojas, e cada entrega tem sua loja vendedora.
    
    * Mudança no modelo preditivo: agora, o entregador tem que passar por todas as lojas primeiros, para então depois fazer as entregas.

--- code/app.py
+++ code/app.py
@@ -12,61 +12,79 @@ c = conn.cursor()
 
 #SQL Functions
 def drop_table():
-    c.execute('DROP TABLE IF EXISTS enderecostable')
+    c.execute("DROP TABLE IF EXISTS enderecostable")
 
 def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS enderecostable (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\
-                                                          endereco TEXT)')
+    c.execute("CREATE TABLE IF NOT EXISTS enderecostable (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, categoria TEXT, endereco TEXT,loja TEXT)")
 
-def create_data(address, id = None):
-    c.execute('INSERT INTO enderecostable(id, endereco) VALUES (?, ?)',(id, address))
+def create_data(category, address, shop=None, id = None):
+    c.execute("INSERT INTO enderecostable(id, categoria, endereco, loja) VALUES (?, ?, ?, ?)", (id, category, address, shop))
     conn.commit()
 
 def view_all_rows():
-    c.execute('SELECT * FROM enderecostable')
+    c.execute("SELECT * FROM enderecostable")
     data = c.fetchall()
     return data
 
 def view_origin():
-    c.execute('SELECT endereco FROM enderecostable WHERE id = 1')
+    c.execute("SELECT endereco FROM enderecostable WHERE categoria = 'origem'")
+    data = c.fetchall()
+    return data
+
+def view_shops():
+    c.execute("SELECT endereco,loja FROM enderecostable WHERE categoria = 'loja'")
     data = c.fetchall()
     return data
 
 def view_addresses():
-    c.execute('SELECT endereco FROM enderecostable WHERE id != 1')
+    c.execute("SELECT endereco, loja FROM enderecostable WHERE categoria = 'entrega'")
+    data = c.fetchall()
+    return data
+
+def view_rows_by_category(category):
+    c.execute("SELECT * FROM enderecostable WHERE categoria = '{}'".format(category))
     data = c.fetchall()
     return data
 
 def delete_origin():
-    c.execute('DELETE FROM enderecostable WHERE id=1')
+    c.execute("DELETE FROM enderecostable WHERE categoria = 'origem'")
     conn.commit()
 
-def delete_address(address):
-    c.execute('DELETE FROM enderecostable WHERE endereco="{}"'.format(address))
+def delete_data_by_address(address):
+    c.execute("DELETE FROM enderecostable WHERE endereco='{}'".format(address))
+    conn.commit()
+
+def delete_data_by_shop(shop):
+    c.execute('DELETE FROM enderecostable WHERE loja="{}"'.format(shop))
     conn.commit()
 
 def get_address_by_id(id):
-    c.execute('SELECT * FROM enderecostable WHERE id = {}'.format(id))
+    c.execute("SELECT * FROM enderecostable WHERE id = {}".format(id))
     data = c.fetchall()
     return data
 
 # def update_origin(address):
 #     c.execute('UPDATE enderecostable SET endereco="{}" WHERE id=1'.format(address))
 #     conn.commit()
+
+
 #MODEL Function
-def run_model(addresses, metric):
+def run_model(addresses, shops_names, metric):
     time.sleep(1)
     st.success('Carregando...')
+    n_shops = len(set(shops_names))
+    # time.sleep(1)
+    # st.success('Carregando...')
     st.markdown("""
     <h3>Legenda:</h3>
     <ul style="list-style-type:none;">
-        <li><span style="color:black;margin-left:0;padding-left:0;">Preto</span> - Endereço de origem</li>
-        <li><span style="color:green;">Verde</span> - Primeira entrega</li>
-        <li><span style="color:red;">Vermelho</span> - Última entrega</li>
-        <li><span style="color:blue;">Azul</span> - Demais entregas</li>
+        <li><span style="color:black;font-weight:bold;">Preto</span> - Endereço de origem</li>
+        <li><span style="color:#2ca02c;font-weight:bold;">Verde</span> - Lojas</li>
+        <li><span style="color:#BA3CC2;font-weight:bold;">Lilás</span> - Entregas</li>
     </ul>
     """, unsafe_allow_html=True)
-    route_map = pipeline_model(addresses, metric)
+
+    route_map = pipeline_model(addresses, shops_names, n_shops, metric)
     filepath = os.path.realpath('map.html')
     route_map.save(filepath)
     webbrowser.open('file://' + filepath)
@@ -92,69 +110,99 @@ def main():
     
     elif choice == 'App':
         st.subheader("Evolution Router App")
-        app_sections = ['Cadastrar Origem', 'Cadastrar Entrega', 'Visualizar Informações', 'Gerar Rota']
+        app_sections = ['Cadastrar Origem', 'Cadastrar Loja', 'Cadastrar Entrega', 'Gerenciar Endereços', 'Gerar Rota']
         applist = st.sidebar.selectbox("Funções do App", app_sections)
         
         if applist == 'Cadastrar Origem':
             st.subheader('Cadastrar endereço de origem')
-            st.warning("Ao cadastrar um novo endereço de origem, o antigo será atualizado.")
-            origem = st.text_input("Insira o endereço de origem:")
+            st.warning("Ao cadastrar um novo endereço de origem, o antigo será removido.")
+            origem_endereco_input = st.text_input("Insira o endereço de origem:")
 
             if st.button("Adicionar origem"):
                 #drop_table()
                 create_table()
-                if get_address_by_id(1):
+                if view_origin():
                     delete_origin()
-                    create_data(origem, id=1)
-                else:
-                    create_data(origem)
+                create_data('origem', origem_endereco_input, None)
                     
                 st.success('Origem adicionada com sucesso.')
 
+        elif applist == 'Cadastrar Loja':
+            st.subheader('Cadastrar endereço de loja')
+
+            loja_loja_input = st.text_input("Digite o nome da loja:")
+            loja_endereco_input = st.text_input("Insira o endereço da loja:")
+            
+            if st.button("Adicionar loja"):
+                create_data('loja', loja_endereco_input, shop=loja_loja_input)
+                st.success('Loja adicionada com sucesso.')
+
         elif applist == 'Cadastrar Entrega':
             st.subheader('Cadastrar endereço de entrega')
-
-            entrega = st.text_input("Insira o endereço de entrega:")
+            lista_nomes_lojas = [i[1] for i in view_shops()]
+            entrega_loja_input = st.selectbox("Escolha a loja vendedora:", lista_nomes_lojas)
+            entrega_endereco_input = st.text_input("Insira o endereço de entrega:")
             if st.button("Adicionar entrega"):
-                create_data(entrega)
-                st.success('Endereço adicionado com sucesso.')
-                time.sleep(2)
+                create_data('entrega', entrega_endereco_input, entrega_loja_input)
+                st.success('Endereço de entrega adicionado com sucesso.')
+                time.sleep(1)
                 st.write('Adicione mais endereços de entrega caso queira aumentar sua rota de entrega.')
         
-        elif applist == 'Visualizar Informações':
+        elif applist == 'Gerenciar Endereços':
             st.subheader('Visualizar informações referentes à sua rota')
-            info_choice = st.radio("Visualizar endereço(s): ", ("origem", "entregas"))
+            info_choice = st.radio("Visualizar: ", ("origem", "loja", "entrega"))
 
             if info_choice == 'origem':
-                st.dataframe(pd.DataFrame({'origem':view_origin()[0]}))
+                st.dataframe(pd.DataFrame({'Endereço da origem':view_origin()[0]}))
             
-            elif info_choice == 'entregas':
-                all_addresses = [i[0] for i in view_addresses()]
-                st.dataframe(pd.DataFrame({'entregas': all_addresses}))
-                address_to_del = st.selectbox("Excluir um endereço:", all_addresses)
+            elif info_choice == 'loja':
+                lojas_nomes = [i[1] for i in view_shops()]
+                lojas_enderecos = [i[0] for i in view_shops()]
+                st.dataframe(pd.DataFrame({'Nome da loja':lojas_nomes, 'Endereço da loja':lojas_enderecos}))
+
+                loja_a_excluir = st.selectbox("Excluir uma loja:", lojas_nomes)
                 if st.button("Excluir"):
-                    delete_address(address_to_del)
-                    st.warning("Endereço excluído: '{}'".format(address_to_del))
-                if st.button("Excluir todos"):
-                    for i in all_addresses:
-                        delete_address(i)
+                    delete_data_by_shop(loja_a_excluir)
+                    st.warning("{} foi excluída.".format(loja_a_excluir))
+                if st.button("Excluir todas as lojas"):
+                    for loja in lojas_nomes:
+                        delete_data_by_shop(loja)
+                    st.warning("Todas as lojas foram excluídas.")
+            
+            elif info_choice == 'entrega':
+                entregas_enderecos = [i[0] for i in view_addresses()]
+                entregas_lojas = [i[1] for i in view_addresses()]
+                st.dataframe(pd.DataFrame({'Endereço de entrega': entregas_enderecos, 'Loja vendedora': entregas_lojas}))
+
+                entrega_a_excluir = st.selectbox("Excluir uma entrega:", entregas_enderecos)
+                if st.button("Excluir"):
+                    delete_data_by_address(entrega_a_excluir)
+                    st.warning("Entrega excluída: '{}'.".format(entrega_a_excluir))
+                if st.button("Excluir todas as entregas"):
+                    for endereco in entregas_enderecos:
+                        delete_data_by_address(endereco)
                     st.warning("Todos os endereços foram excluídos.")
    
 
         elif applist == 'Gerar Rota':
             st.subheader('Gere sua rota otimizada baseada nos endereços registrados')
-            metric_radio = st.radio("Seleciona a métrica de otimização: ", ("distância total", "tempo total"))
-            metric = NotImplementedError
-            if metric_radio == 'distância total':
-                    metric = 'distance'
-            elif metric_radio == 'tempo total':
-                    metric = 'time'
-            if st.button("Gerar rota") and metric is not None:
-                result = view_all_rows()
-                clean_db = pd.DataFrame(result, columns=['Id', 'Enderecos'])
-                enderecos = clean_db['Enderecos']
+            metrica_radio = st.radio("Seleciona a métrica de otimização: ", ("distância total", "tempo total"))
+            metrica = 'distance'
+            if metrica_radio == 'distância total':
+                    metrica = 'distance'
+            elif metrica_radio == 'tempo total':
+                    metrica = 'time'
+
+            if st.button("Gerar rota"):
+                colunas = ['Id', 'Categoria', 'Endereco', 'Loja']
+                df_origem = pd.DataFrame(view_rows_by_category('origem'), columns=colunas)
+                df_lojas = pd.DataFrame(view_rows_by_category('loja'), columns=colunas)
+                df_entregas = pd.DataFrame(view_rows_by_category('entrega'), columns=colunas)
+
+                enderecos_model = [*df_origem['Endereco'], *df_lojas['Endereco'], *df_entregas['Endereco']]
+                nome_lojas_model = [*df_lojas['Loja'], *df_entregas['Loja']]
                 
-                run_model(enderecos, metric)
+                run_model(enderecos_model, nome_lojas_model, metrica)
 
 
 
--- code/model.py
+++ code/model.py
@@ -7,120 +7,84 @@ from copy import copy
 import folium
 from folium.plugins import BeautifyIcon
 
-def pipeline_model(addresses, metric):
-    geolocator = Nominatim(user_agent="rickchaves")
-    address_names = addresses 
-
-    address_coordinates = {i:[geolocator.geocode(k).latitude, geolocator.geocode(k).longitude] for i,k in enumerate(address_names)}
-    address_distances_dict = save_distances(address_names, address_coordinates, metric)
-    current_generation = create_generation(list(address_coordinates.keys()), address_distances_dict, population=500)
-    _ , best_guess = evolve_to_solve(current_generation, 100, 150, 70, 0.5, 3, 5, address_distances_dict, verbose=True)
-    map = plot_map(geolocator, best_guess, address_coordinates, address_names)
-    #map.save('map.html')
-    return map
+def pipeline_model(addresses, shops_names, n_shops, metric):
+    '''
+    addresses -> todos os endereços (origem, lojas, entregas)
+    shops_names -> nome das lojas (lojas, entregas)
+    n_shops -> quantidade de lojas
+    metric -> métrica para calcular o resultado (distancia ou tempo)
     
+    '''
+    geolocator = Nominatim(user_agent='rickchaves')
+    coordinates = {i:[geolocator.geocode(k).latitude, geolocator.geocode(k).longitude] for i,k in enumerate(addresses)}
+    metric_results = save_distances(addresses, coordinates, metric)
+    current_generation = create_generation(list(coordinates.keys()), n_shops, population=500)
+    _ , best_guess = evolve_to_solve(current_generation, metric_results, 100, 150, 70, 0.5, 3, 5, verbose=True)
+    m = plot_map(geolocator, best_guess, coordinates, addresses, shops_names, n_shops)
+    #map.save('map.html')
+    return m
 
-def get_distance(address_1, address_2, address_coordinates, metric, API_KEY='Wo4TQp6dReCezt0qVyIlSgAWTfex3lzUtcRcw0DN-uM', mode='fastest', vehicle='car', traffic='enabled'):
+def get_distance(address_1, address_2, coordinates, metric, API_KEY='Wo4TQp6dReCezt0qVyIlSgAWTfex3lzUtcRcw0DN-uM', mode='fastest', vehicle='car', traffic='enabled'):
     """
-    Given two address, this calculates this distance between them
+    Given two address, this calculates the selected metric between then
     """
-    c1 = address_coordinates[address_1]
-    c2 = address_coordinates[address_2]
+    c1 = coordinates[address_1]
+    c2 = coordinates[address_2]
     request_template = f'https://route.ls.hereapi.com/routing/7.2/calculateroute.json?apiKey={API_KEY}&waypoint0=geo!{c1[0]},{c1[1]}&waypoint1=geo!{c2[0]},{c2[1]}&mode={mode};{vehicle};traffic:{traffic}'
     
     r = requests.get(request_template)
     request_json = json.loads(r.text)
     
     if metric == 'distance':
-        distance = request_json['response']['route'][0]['summary']['distance']/1000
+        result = request_json['response']['route'][0]['summary']['distance']/1000
     elif metric == 'time':
-        distance = request_json['response']['route'][0]['summary']['travelTime']/60
-    return distance
+        result = request_json['response']['route'][0]['summary']['travelTime']/60
+    return result
 
 def save_distances(addresses, coordinates, metric):
-    distances_dict = {}
+    metric_results = {}
     l = len(addresses)
     for i in range(l):
         for j in range(l):
             if i != j:
-                distances_dict[(i, j)] = get_distance(i, j, coordinates, metric)
-    return distances_dict
+                metric_results[(i, j)] = get_distance(i, j, coordinates, metric)
+    return metric_results
 
-def create_guess(addresses):
+def create_guess(coordinates_keys, n_shops):
     """
     Creates a possible route between all adresses, returning to the original.
     Input: List of Adr
     """
-    guess = copy(addresses)
+    guess = copy(coordinates_keys)
     np.random.shuffle(guess)
-    while guess[0] != 0:
-        np.random.shuffle(guess)
-    guess.append(guess[0])
+
+    i = 0
+    while i < n_shops:
+        i += 1
+        if guess[i] not in range(1, n_shops + 1) or guess[0] != 0:
+            np.random.shuffle(guess)
+            i = 0
+    #guess.append(guess[0])
     return list(guess)
 
-def plot_address(address_coordinates, annotate=True):
-    """
-    Makes a plot of all addresses.
-    Input: address_coordinates; dictionary of all addresses and their coordinates in (x,y) format
-    """
-    names = []
-    x = []
-    y = []
-    plt.figure(dpi=250)
-    for ix, coord in address_coordinates.items():
-        names.append(ix)
-        x.append(coord[0])
-        y.append(coord[1])
-        if annotate:
-            plt.annotate(ix, xy=(coord[0], coord[1]), xytext=(20, -20),
-                        textcoords='offset points', ha='right', va='bottom',
-                        bbox=dict(boxstyle='round,pad=0.5', fc='w', alpha=0.5),
-                        arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
-    plt.scatter(x,y,c='r',marker='o')
-
-def plot_guess(address_coordinates, guess, guess_in_title=True):
-    """
-    Takes the coordinates of the cities and the guessed path and
-    makes a plot connecting the cities in the guessed order
-    Input:
-    city_coordinate: dictionary of city id, (x,y)
-    guess: list of ids in order
-    """
-    plot_address(address_coordinates)
-    for ix, _ in enumerate(guess[:-1]):
-        x = [address_coordinates[guess[ix]][0],address_coordinates[guess[ix+1]][0]]
-        y = [address_coordinates[guess[ix]][1],address_coordinates[guess[ix+1]][1]]
-        if ix == 0:
-            plt.plot(x,y,'g--',lw=1)
-        elif ix == len(guess)-2:
-            plt.plot(x,y,'r--',lw=1)
-        else:
-            plt.plot(x,y,'c--',lw=1)
-    plt.scatter(address_coordinates[guess[0]][0],address_coordinates[guess[0]][1], marker='x', c='b')
-    if guess_in_title:
-        plt.title("Current Guess: [%s]"%(','.join([str(x) for x in guess])))
-    else:
-        print("Current Guess: [%s]"%(','.join([str(x) for x in guess])))
-
-def create_generation(addresses, address_distances_dict, population=100):
+def create_generation(coordinates_keys, n_shops, population=100):
     """
     Makes a list of guessed adress orders given a list of address IDs.
     Input:
     adresses: list of address ids
     population: how many guesses to make
     """
-    generation = [create_guess(addresses) for _ in range(population)]
+    generation = [create_guess(coordinates_keys, n_shops) for _ in range(population)]
     return generation
 
-def distance_between_address(address_1, address_2, address_distances_dict, API_KEY='Wo4TQp6dReCezt0qVyIlSgAWTfex3lzUtcRcw0DN-uM', mode='fastest', vehicle='car', traffic='disabled'):
-
+def distance_between_address(address_1, address_2, metric_results, API_KEY='Wo4TQp6dReCezt0qVyIlSgAWTfex3lzUtcRcw0DN-uM', mode='fastest', vehicle='car', traffic='disabled'):
     """
     Given two address, this calculates this distance between them
     """
     
-    return address_distances_dict[(address_1, address_2)]
+    return metric_results[(address_1, address_2)]
 
-def fitness_score(guess, address_distances_dict):
+def fitness_score(guess, metric_results):
     """
     Loops through the adresses in the guesses order and calculates
     how much distance the path would take to complete a loop.
@@ -128,22 +92,21 @@ def fitness_score(guess, address_distances_dict):
     """
     score = 0
     for ix, address_id in enumerate(guess[:-1]):
-        score += distance_between_address(address_id, guess[ix+1], address_distances_dict)
+        score += distance_between_address(address_id, guess[ix+1], metric_results)
     return score
 
-def check_fitness(guesses, address_distances_dict):
+def check_fitness(guesses, metric_results):
     """
     Goes through every guess and calculates the fitness score. 
     Returns a list of tuples: (guess, fitness_score)
     """
     fitness_indicator = []
     for guess in guesses:
-        fitness_indicator.append((guess, fitness_score(guess, address_distances_dict)))
+        fitness_indicator.append((guess, fitness_score(guess, metric_results)))
     
     return fitness_indicator
 
-def get_breeders_from_generation(guesses, address_distances_dict, take_best_N=10, take_random_N=5, verbose=False, mutation_rate=0.1):
-
+def get_breeders_from_generation(guesses, metric_results, take_best_N=10, take_random_N=5, verbose=False, mutation_rate=0.1):
     """
     This sets up the breeding group for the next generation. You have
     to be very careful how many breeders you take, otherwise your
@@ -151,7 +114,7 @@ def get_breeders_from_generation(guesses, address_distances_dict, take_best_N=10
     in the make_children function must be tuned to avoid exponential growth or decline!
     """
     # First, get the top guesses from last time
-    fit_scores = check_fitness(guesses, address_distances_dict)
+    fit_scores = check_fitness(guesses, metric_results)
     sorted_guesses = sorted(fit_scores, key=lambda x: x[1]) # sorts so lowest is first, which we want
     new_generation = [x[0] for x in sorted_guesses[:take_best_N]]
     best_guess = new_generation[0]
@@ -189,7 +152,7 @@ def make_child(parent1, parent2):
                 if gene2 not in child:
                     child[ix] = gene2
                     break
-    child[-1] = child[0]
+    #child[-1] = child[0]
     return child
 
 def make_children(old_generation, children_per_couple=1):
@@ -209,8 +172,8 @@ def make_children(old_generation, children_per_couple=1):
             next_generation.append(make_child(parent, old_generation[-ix-1]))
     return next_generation
 
-def evolve_to_solve(current_generation, max_generations, take_best_N, take_random_N,
-                    mutation_rate, children_per_couple, print_every_n_generations, address_distances_dict, verbose=False):
+def evolve_to_solve(current_generation, metric_results, max_generations, take_best_N, take_random_N,
+                    mutation_rate, children_per_couple, print_every_n_generations, verbose=False):
     """
     Takes in a generation of guesses then evolves them over time using our breeding rules.
     Continue this for "max_generations" times.
@@ -230,49 +193,50 @@ def evolve_to_solve(current_generation, max_generations, take_best_N, take_rando
     fitness_tracking = []
     for i in range(max_generations):
         if verbose and not i % print_every_n_generations and i > 0:
-            print("Generation %i: "%i, end='')
-            print(len(current_generation))
-            print("Current Best Score: ", fitness_tracking[-1])
+            #print("Generation %i: "%i, end='')
+            #print(len(current_generation))
+            #print("Current Best Score: ", fitness_tracking[-1])
             is_verbose = True
         else:
             is_verbose = False
-        breeders, best_guess = get_breeders_from_generation(current_generation, address_distances_dict, 
+        breeders, best_guess = get_breeders_from_generation(current_generation, metric_results, 
                                                             take_best_N=take_best_N, take_random_N=take_random_N, 
                                                             verbose=is_verbose, mutation_rate=mutation_rate)
-        fitness_tracking.append(fitness_score(best_guess, address_distances_dict))
+        fitness_tracking.append(fitness_score(best_guess, metric_results))
         current_generation = make_children(breeders, children_per_couple=children_per_couple)
     return fitness_tracking, best_guess
 
-def plot_map(geolocator, best_guess, address_coordinates, address_names):
+
+def plot_map(geolocator, best_guess, coordinates, addresses, shops_names, n_shops):
     location = geolocator.geocode("Rio de Janeiro, RJ, Brazil")
     m = folium.Map(location = [location.latitude, location.longitude], zoom_start = 11)
-
-
-    for ix,i in enumerate(best_guess[:-1]):
-        colors = dict()
-
+    
+    for ix,i in enumerate(best_guess):
+        popup = 'None'
+        color = '#000000'
+    
         if ix == 0:
-            colors[ix] = '#000000'
+            popup = folium.Popup('Origem')
+            color ='#000000'
+        elif ix > 0 and ix <= n_shops:
+            popup = folium.Popup('{}a Parada<br>Abastecimento na Loja {}'.format(ix, shops_names[i-1]), max_width=200)
+            color = '#2ca02c'
         else:
-            if ix==1:
-                colors[ix] = '#2ca02c'
-            elif ix== len(best_guess)-2:
-                colors[ix] = '#d62728'
-            else:
-                colors[ix] = '#17becf'
-
+            popup = folium.Popup('{}a Parada<br>Entrega da Loja {}'.format(ix, shops_names[i-1]), max_width=200)
+            color = '#BA3CC2'
+    
         icon_number = BeautifyIcon(
-            border_color=colors[ix],
-            text_color='#000000',
-            number=ix,
-            iconSize = [30,30],
-            inner_icon_style='margin-top:10;'
-        )
+        border_color=color,
+        text_color='#000000',
+        number=ix,
+        icon_size=[30,30],
+        inner_icon_style='margin_top:10;')
 
         folium.Marker(
-            location=[address_coordinates[i][0], address_coordinates[i][1]],
-            tooltip=address_names[i],
-            icon=icon_number
-        ).add_to(m)
-
+        location=[coordinates[i][0], coordinates[i][1]],
+        popup=popup,
+        tooltip=addresses[i],
+        icon=icon_number).add_to(m)
+        
     return m
+
